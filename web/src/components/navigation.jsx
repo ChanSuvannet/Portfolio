b29df2c52c1218d8613d30d811e0d8f5198b041b
CHAN Suvannet <chansuvannet999@gmail.com>
2025-05-03 13:53:04 +0700

    update image

--- web/src/components/navigation.jsx
+++ web/src/components/navigation.jsx
@@ -1,4 +1,4 @@
-import { IconBooks, IconBriefcase, IconCertificate, IconFlame, IconMenu2, IconMichelinStarGreen, IconX } from "@tabler/icons-react";
+import { IconBooks, IconBriefcase, IconCertificate, IconMenu2, IconMichelinStarGreen, IconX } from "@tabler/icons-react";
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 import { cn } from "../utils/cn.ts";
@@ -76,18 +76,23 @@ const Navigation = () => {
               href="/"
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
-              className="block w-10 h-10"
+              className="block w-[100px] h-[100px] mt-[22px]"
               aria-label="Home"
             >
               {isHovered ? (
                 <img
                   src={HeaderImageHover}
                   alt="Hover Icon"
-                  className="w-9 h-9 mt-[14px] sm:mt-[14px] md:mt-0"
+                  className="w-[50px]"
                   onError={(e) => (e.target.style.display = "none")} // Hide broken image
                 />
               ) : (
-                <IconFlame className="w-9 h-9 text-gray-500 mt-[14px] sm:mt-[14px] md:mt-0" aria-hidden="true" />
+                <img
+                  src="https://camo.githubusercontent.com/5046cb083418fd1922b7f5990e594c3bb06f5d87e5516cd8839ae0aa48b3aec4/68747470733a2f2f696d616765732e73717561726573706163652d63646e2e636f6d2f636f6e74656e742f76312f3537363966633430316236333162616231616464623261622f313534313538303631313632342d5445363451474b524a4738535741495553374e532f6b6531375a77644742546f6464493870446d34386b506f73776c7a6a53564d4d2d53784f703743563539425a772d7a505067646e346a557756634a45315a7657515578776b6d794578676c4e714770304976544a5a616d574c49327a76595748384b332d735f3479737a63703272795449304871544f6161556f68724938504936465879386339505774426c7141566c555335697a7064634958445a71445976707252715a32395077306f2f636f64696e672d667265616b2e676966"
+                  alt="Logo"
+                  className="w-[100px]"
+                  onError={(e) => (e.target.style.display = "none")} // Hide broken image
+                />
               )}
             </a>
           </div>
@@ -97,7 +102,7 @@ const Navigation = () => {
                 onClick={() => setOpen(!open)}
                 id="navbarToggler"
                 className={cn(
-                  "absolute flex justify-end right-4 top-1/2 -translate-y-1/2 mt-[8px] rounded-lg lg:hidden",
+                  "absolute flex justify-end right-4 top-1/2 -translate-y-1/2 mt-[1px] rounded-lg lg:hidden",
                   open && "navbarTogglerActive"
                 )}
                 aria-label={open ? "Close menu" : "Open menu"}
